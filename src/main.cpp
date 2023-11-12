1e79c49a03e9f249ba73f339004430863d064601
kopiotrek <kopiotrek@gmail.com>
2023-11-12 21:18:30 +0100

    dodano czytanie komend z pliku

--- src/main.cpp
+++ src/main.cpp
@@ -79,76 +79,9 @@ void execCommands( const char* fname ){
 int main()
 {
 
-  AbstractComChannel comChannel();
-  AbstractScene scene();
-  // void *pLibHnd_Move = dlopen("libInterp4Move.so",RTLD_LAZY);
-  // void *pLibHnd_Pause = dlopen("libInterp4Pause.so",RTLD_LAZY);
-  // void *pLibHnd_Rotate = dlopen("libInterp4Rotate.so",RTLD_LAZY);
-  // void *pLibHnd_Set = dlopen("libInterp4Set.so",RTLD_LAZY);
-
-
-
-  // AbstractInterp4Command *(*pCreateCmd_Move)(void);
-  // AbstractInterp4Command *(*pCreateCmd_Pause)(void);
-  // AbstractInterp4Command *(*pCreateCmd_Rotate)(void);
-  // AbstractInterp4Command *(*pCreateCmd_Set)(void);
-  std::vector<std::string> libNames = {"libInterp4Move.so", "libInterp4Pause.so", "libInterp4Rotate.so", "libInterp4Set"};
+  std::vector<std::string> libNames = {"libInterp4Move.so", "libInterp4Pause.so", "libInterp4Rotate.so", "libInterp4Set.so"};
   addLibs(libNames);
 
-  // void *pFun;
-  // if (!pLibHnd_Move) {
-  //   cerr << "!!! Brak biblioteki: Interp4Move.so" << endl;
-  //   return 1;
-  // }
-
-
-  // pFun = dlsym(pLibHnd_Move,"CreateCmd");
-  // if (!pFun) {
-  //   cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
-  //   return 1;
-  // }
-  // pCreateCmd_Move = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
-  // pFun = dlsym(pLibHnd_Pause,"CreateCmd");
-  // if (!pFun) {
-  //   cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
-  //   return 1;
-  // }
-  // pCreateCmd_Pause = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
-  // pFun = dlsym(pLibHnd_Rotate,"CreateCmd");
-  // if (!pFun) {
-  //   cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
-  //   return 1;
-  // }
-  // pCreateCmd_Rotate = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
-  // pFun = dlsym(pLibHnd_Set,"CreateCmd");
-  // if (!pFun) {
-  //   cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
-  //   return 1;
-  // }
-  // pCreateCmd_Set = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
-
-
-
-  // AbstractInterp4Command* pCmd[4];
-  // pCmd[0]=pCreateCmd_Move();
-  // pCmd[1]=pCreateCmd_Pause();
-  // pCmd[2]=pCreateCmd_Rotate();
-  // pCmd[3]=pCreateCmd_Set();
-
-// for (size_t i = 0; i < 4; i++)
-// {
-//   printPluginInfo(pCmd[i]);
-//   delete pCmd[i];
-// }
-
-  // dlclose(pLibHnd_Move);
-  // dlclose(pLibHnd_Pause);
-  // dlclose(pLibHnd_Rotate);
-  // dlclose(pLibHnd_Set);
-
-  // istringstream streamls;
-  // ExecPreprocesor("commands.cmd", streamls);
   execCommands("commands.cmd");
 
-  // cout<<streamls.str()<<endl;
 }
